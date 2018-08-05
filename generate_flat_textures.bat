840d92dd1874f2bff28e9a19aed9198f5c3c14d7
JarateKing <scmacintosh@upei.ca>
2018-08-05 18:57:07 -0300

    made generate_flat_textures flatten textures

--- generate_flat_textures.bat
+++ generate_flat_textures.bat
@@ -1,5 +1,14 @@
 for /F "tokens=*" %%A in (dev\lists\flat.txt) do (
 	mkdir %%A
 	dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
+	
+	for %%f in (%%A\*.vtf) do (
+		dev\batch\vtf-to-png.bat %%f
+	)
+	for %%f in (%%A\*.png) do (
+		dev\batch\average-color.bat %%f
+		dev\batch\png-to-vtf.bat %%f
+		del %%f
+	)
 )
 pause

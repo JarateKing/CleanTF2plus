88ce8603dc34d9f816dea9f48e4e4ad6616ea959
JarateKing <scmacintosh@upei.ca>
2018-08-09 21:41:16 -0300

    fixed model removal using wrong vpk

--- generate_model_removal.bat
+++ generate_model_removal.bat
@@ -6,7 +6,7 @@ for /F "tokens=*" %%A in (dev\lists\modelremoval.txt) do (
 		copy dev\ref\null.vtx %%A
 	) || (
 		mkdir %%A
-		dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
+		dev\HLExtract.exe -p "../../tf2_misc_dir.vpk" -d %%A\..\ -e %%A -m -v -s
 		for %%f in (%%A\*.vtx) do copy dev\ref\null.vtx %%f
 	)
 )

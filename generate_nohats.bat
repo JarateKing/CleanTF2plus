af25f7dcbe59274e4eb3bafaa51f05642fa40f52
JarateKing <scmacintosh@upei.ca>
2018-08-11 10:09:39 -0300

    made generate nohats work with subdirectories

--- generate_nohats.bat
+++ generate_nohats.bat
@@ -7,12 +7,9 @@ for /F "tokens=*" %%A in (dev\lists\nohats.txt) do (
 	) || (
 		mkdir %%A
 		dev\HLExtract.exe -p "../../tf2_misc_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for %%f in (%%A\*) do (
-			Echo.%%f | findstr /c:".vtx">nul && (
-				copy dev\ref\null.vtx %%f
-			) || (
-				del %%f
-			)
-		)
+		for /f "delims=" %%f in ('dir /s /b %%A\*.vtx') do copy dev\ref\null.vtx "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.mdl') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.vvd') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.phy') do del "%%f"
 	)
 )

233b09faa632dd0afb6f9875a8ad8ac6466c7e9c
JarateKing <scmacintosh@upei.ca>
2018-08-22 20:09:16 -0300

    made model removal use same subfolder system as nohats

--- generate_model_removal.bat
+++ generate_model_removal.bat
@@ -7,6 +7,10 @@ for /F "tokens=*" %%A in (dev\lists\modelremoval.txt) do (
 	) || (
 		mkdir %%A
 		dev\HLExtract.exe -p "../../tf2_misc_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for %%f in (%%A\*.vtx) do copy dev\ref\null.vtx %%f
+		for /f "delims=" %%f in ('dir /s /b %%A\*.mdl') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.vvd') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.phy') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.sw.vtx') do del "%%f"
+		for /f "delims=" %%f in ('dir /s /b %%A\*.vtx') do copy dev\ref\null.vtx "%%f"
 	)
 )

8842dc77877a9de968a8a9bf182c7974ef08a9a9
JarateKing <scmacintosh@upei.ca>
2018-08-26 13:06:52 -0300

    fixed context stuff for nodraw materials generator

--- generate_nodraw_textures.bat
+++ generate_nodraw_textures.bat
@@ -4,19 +4,19 @@ for /F "tokens=*" %%A in (dev\lists\nodraw.txt) do (
 	Echo.%%A | findstr /c:".">nul && (
 		mkdir %%A\..\
 		dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		dev\batch\vtf-to-tga.bat %%A
+		call dev\batch\vtf-to-tga.bat %%A
 		for %%f in (%%A\..\*.tga) do (
-			dev\batch\clear-image.bat %%f
-			dev\batch\tga-to-vtf.bat %%f
+			call dev\batch\clear-image.bat %%f
+			call dev\batch\tga-to-vtf.bat %%f
 			del %%f
 		)
 	) || (
 		mkdir %%A
 		dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for %%f in (%%A\*.vtf) do dev\batch\vtf-to-tga.bat %%f
+		for %%f in (%%A\*.vtf) do call dev\batch\vtf-to-tga.bat %%f
 		for %%f in (%%A\*.tga) do (
-			dev\batch\clear-image.bat %%f
-			dev\batch\tga-to-vtf.bat %%f
+			call dev\batch\clear-image.bat %%f
+			call dev\batch\tga-to-vtf.bat %%f
 			del %%f
 		)
 	)

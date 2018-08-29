1e31afb67cbef81ce74118734a7bbe23c9c00f71
JarateKing <scmacintosh@upei.ca>
2018-08-29 19:25:11 -0300

    made generate_flat_textures also do some hl2 textures

--- generate_flat_textures.bat
+++ generate_flat_textures.bat
@@ -1,1 +1,2 @@
 call dev\generators\textures_flat.bat dev\lists\flat.txt "../../tf2_textures_dir.vpk"
+call dev\generators\textures_flat.bat dev\lists\flat_hl2.txt "../../../hl2/hl2_textures_dir.vpk"

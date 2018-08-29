58c5dd7357d83fe48d1efadc13c9c1378f98d2ee
JarateKing <scmacintosh@upei.ca>
2018-08-29 20:34:06 -0300

    made generate_flat_textures.bat resize to 1x1 by default

--- generate_flat_textures.bat
+++ generate_flat_textures.bat
@@ -1,2 +1,2 @@
-call dev\generators\textures_flat.bat dev\lists\flat.txt "../../tf2_textures_dir.vpk"
-call dev\generators\textures_flat.bat dev\lists\flat_hl2.txt "../../../hl2/hl2_textures_dir.vpk"
+call dev\generators\textures_flat.bat dev\lists\flat.txt "../../tf2_textures_dir.vpk" 1
+call dev\generators\textures_flat.bat dev\lists\flat_hl2.txt "../../../hl2/hl2_textures_dir.vpk" 1

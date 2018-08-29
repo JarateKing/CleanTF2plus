37c24b7dcae6728d388c209db56d952abc424c8a
JarateKing <scmacintosh@upei.ca>
2018-08-29 18:55:39 -0300

    made generate_flat_textures use generic bat

--- generate_flat_textures.bat
+++ generate_flat_textures.bat
@@ -1,26 +1,2 @@
-:: go through entire flat.txt list
-for /F "tokens=*" %%A in (dev\lists\flat.txt) do (
-	:: do the first if it's a file, second if it's a folder
-	Echo.%%A | findstr /c:".">nul && (
-		mkdir %%A\..\
-		dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		call dev\batch\vtf-to-tga.bat %%A
-		for %%f in (%%A\..\*.tga) do (
-			call dev\batch\average-color.bat %%f
-			call dev\batch\resize-image.bat %%f 1
-			call dev\batch\tga-to-vtf.bat %%f
-			del %%f
-		)
-	) || (
-		mkdir %%A
-		dev\HLExtract.exe -p "../../tf2_textures_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for %%f in (%%A\*.vtf) do call dev\batch\vtf-to-tga.bat %%f
-		for %%f in (%%A\*.tga) do (
-			call dev\batch\average-color.bat %%f
-			call dev\batch\resize-image.bat %%f 1
-			call dev\batch\tga-to-vtf.bat %%f
-			del %%f
-		)
-		for /d %%d in (%%A\*) do rmdir /s /q %%d
-	)
-)
+call dev\generators\textures_flat.bat dev\lists\flat.txt
+pause

c64c40aa9746ecedaa0bdec98bb3c2c1bd17c09e
JarateKing <scmacintosh@upei.ca>
2018-08-29 18:59:06 -0300

    made generate_nohats.bat use generic bat

--- generate_nohats.bat
+++ generate_nohats.bat
@@ -1,16 +1,1 @@
-:: go through entire nohats.txt list
-for /F "tokens=*" %%A in (dev\lists\nohats.txt) do (
-	:: do the first if it's a file, second if it's a folder
-	Echo.%%A | findstr /c:".">nul && (
-		mkdir %%A\..\
-		copy dev\ref\null.vtx %%A
-	) || (
-		mkdir %%A
-		dev\HLExtract.exe -p "../../tf2_misc_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for /f "delims=" %%f in ('dir /s /b %%A\*.mdl') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.vvd') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.phy') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.sw.vtx') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.vtx') do copy dev\ref\null.vtx "%%f"
-	)
-)
+call dev\generators\models_null.bat dev\lists\nohats.txt

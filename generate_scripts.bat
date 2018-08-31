2e1d525836ce15f25b0fdec4cb80af70cbdc38b2
JarateKing <scmacintosh@upei.ca>
2018-08-30 21:42:20 -0300

    made generate_scripts change text for surfaceproperties

--- generate_scripts.bat
+++ generate_scripts.bat
@@ -1,4 +1,4 @@
 call dev\generators\scripts_copy.bat extra_models.txt
 call dev\generators\scripts_copy.bat soundscapes_manifest.txt
 call dev\generators\scripts_copy.bat surfaceproperties_manifest.txt
-call dev\generators\scripts_copy.bat surfaceproperties.txt
+call dev\generators\scripts_find_and_replace.bat surfaceproperties.txt "REPLACEME" " "

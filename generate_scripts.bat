f80167a32682da76a773bd9be9ae05a2f64873f0
JarateKing <scmacintosh@upei.ca>
2018-08-30 21:06:12 -0300

    added generate_scripts batch file

---
+++ generate_scripts.bat
@@ -0,0 +1,4 @@
+call dev\generators\scripts_copy.bat extra_models.txt
+call dev\generators\scripts_copy.bat soundscapes_manifest.txt
+call dev\generators\scripts_copy.bat surfaceproperties_manifest.txt
+call dev\generators\scripts_copy.bat surfaceproperties.txt

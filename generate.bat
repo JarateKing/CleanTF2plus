7809ba6e9e24398d591cf27a785be3d2fc9ff6b3
JarateKing <scmacintosh@upei.ca>
2018-09-02 09:12:37 -0300

    added base decision structure of generate.bat

---
+++ generate.bat
@@ -0,0 +1,14 @@
+:FLAT
+set /P c=would you like flat materials? Y/N
+if /I "%c%" EQU "Y" goto :FLAT_GEN
+if /I "%c%" EQU "N" goto :END
+echo "invalid input"
+goto :FLAT
+
+:FLAT_GEN
+echo "generating flat textures"
+goto :END
+
+:END
+echo "thank you for using Clean TF2+"
+pause

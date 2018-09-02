45e26f42d308ba9f659e38d739e12e7ab789ee1d
JarateKing <scmacintosh@upei.ca>
2018-09-02 09:05:20 -0300

    made generate_all not use other root scripts

--- generate_all.bat
+++ generate_all.bat
@@ -1,5 +1,18 @@
-call generate_flat_textures.bat
-call generate_model_removal.bat
-call generate_nodraw_textures.bat
-call generate_nohats.bat
-call generate_scripts.bat
+:: generate_flat_textures
+call dev\generators\textures_flat.bat dev\lists\flat.txt "../../tf2_textures_dir.vpk" 1
+call dev\generators\textures_flat.bat dev\lists\flat_hl2.txt "../../../hl2/hl2_textures_dir.vpk" 1
+
+:: generate_nohats
+call dev\generators\models_null.bat dev\lists\nohats.txt
+
+:: generate_nodraw_textures
+call dev\generators\textures_nodraw.bat dev\lists\nodraw.txt
+
+:: generate_model_removal
+call dev\generators\models_null.bat dev\lists\modelremoval.txt
+
+:: generate_scripts
+call dev\generators\scripts_copy.bat extra_models.txt
+call dev\generators\scripts_copy.bat soundscapes_manifest.txt
+call dev\generators\scripts_copy.bat surfaceproperties_manifest.txt
+call dev\generators\scripts_find_and_replace.bat surfaceproperties.txt "REPLACEME" " "

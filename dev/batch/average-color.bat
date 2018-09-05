229a79f579c6acbd47074c01b2485203db59d6ca
JarateKing <scmacintosh@upei.ca>
2018-09-05 12:10:43 -0300

    made batch files use portable imagemagick exe's

--- dev/batch/average-color.bat
+++ dev/batch/average-color.bat
@@ -1,3 +1,3 @@
-dev\magick.exe convert %1 -resize 1x1 -resize 1024x1024 %1.tmp.tga
-dev\magick.exe composite %1.tmp.tga %1 -compose Src %1
+dev\convert.exe %1 -resize 1x1 -resize 1024x1024 %1.tmp.tga
+dev\composite.exe %1.tmp.tga %1 -compose Src %1
 del %1.tmp.tga

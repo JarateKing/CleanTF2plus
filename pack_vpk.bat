e52a1ff8eabc6ae567f932ee7aa8b039543a0b88
JarateKing <scmacintosh@upei.ca>
2018-09-14 12:47:59 -0300

    added simple generate vpk script

---
+++ pack_vpk.bat
@@ -0,0 +1,10 @@
+mkdir cleantf2
+move materials cleantf2\
+move models cleantf2\
+move scripts cleantf2\
+..\..\..\bin\vpk.exe -M cleantf2
+move cleantf2\materials .\
+move cleantf2\models .\
+move cleantf2\scripts .\
+rmdir cleantf2
+pause

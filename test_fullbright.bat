980a3bfd0db2a919936a5cd7134a565f4805ef95
JarateKing <scmacintosh@upei.ca>
2018-09-18 16:39:15 -0300

    fixed minor typo

--- test_fullbright.bat
+++ test_fullbright.bat
@@ -1,3 +1,3 @@
-call dev\generators\materials_fullbright.bat dev\lists\fullbright.txt "../..//tf2_misc_dir.vpk"
+call dev\generators\materials_fullbright.bat dev\lists\fullbright.txt "../../tf2_misc_dir.vpk"
 call dev\generators\materials_fullbright.bat dev\lists\fullbright_hl2.txt "../../../hl2/hl2_misc_dir.vpk"
 pause

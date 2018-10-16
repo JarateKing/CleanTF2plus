2b87798c2fa5779a31edfc05deb6759f67cdcb1f
JarateKing <scmacintosh@upei.ca>
2018-10-16 13:52:34 -0300

    sped up models_null generator

--- dev/generators/models_null.bat
+++ dev/generators/models_null.bat
@@ -3,12 +3,13 @@ for /F "tokens=*" %%A in (%1) do (
 		mkdir %%A\..\
 		copy dev\ref\null.vtx %%A
 	) || (
-		mkdir %%A
-		dev\HLExtract.exe -p "../../tf2_misc_dir.vpk" -d %%A\..\ -e %%A -m -v -s
-		for /f "delims=" %%f in ('dir /s /b %%A\*.mdl') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.vvd') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.phy') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.sw.vtx') do del "%%f"
-		for /f "delims=" %%f in ('dir /s /b %%A\*.vtx') do copy dev\ref\null.vtx "%%f"
+		"../../../bin/vpk.exe" l "../../tf2_misc_dir.vpk" | findstr "%%A" | findstr ".vtx" | findstr /V ".sw.vtx" > vpk_list.txt
+	
+		for /F "tokens=*" %%F in (vpk_list.txt) do (
+			mkdir "%%F\..\"
+			copy dev\ref\null.vtx "%%F"
+		)
+		
+		del vpk_list.txt
 	)
 )

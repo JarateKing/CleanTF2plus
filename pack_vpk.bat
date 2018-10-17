a83543fc0a3e19ebe01470fa8b1ecd842a9ed42d
JarateKing <scmacintosh@upei.ca>
2018-10-17 18:25:27 -0300

    changed echoes for vpk packing

--- pack_vpk.bat
+++ pack_vpk.bat
@@ -1,14 +1,19 @@
-mkdir cleantf2
-move cfg cleantf2\
-move materials cleantf2\
-move models cleantf2\
-move scripts cleantf2\
-copy dev\ref\vpk.txt cleantf2\readme.txt
-..\..\..\bin\vpk.exe -M cleantf2
-del cleantf2\readme.txt
-move cleantf2\cfg .\
-move cleantf2\materials .\
-move cleantf2\models .\
-move cleantf2\scripts .\
-rmdir cleantf2
+@echo off
+echo preparing folder structure
+mkdir cleantf2 > nul 2> nul
+move cfg cleantf2\ > nul 2> nul
+move materials cleantf2\ > nul 2> nul
+move models cleantf2\ > nul 2> nul
+move scripts cleantf2\ > nul 2> nul
+copy dev\ref\vpk.txt cleantf2\readme.txt > nul 2> nul
+echo creating vpk
+..\..\..\bin\vpk.exe -M cleantf2 > nul 2> nul
+echo regenerating folder structure
+del cleantf2\readme.txt > nul 2> nul
+move cleantf2\cfg .\ > nul 2> nul
+move cleantf2\materials .\ > nul 2> nul
+move cleantf2\models .\ > nul 2> nul
+move cleantf2\scripts .\ > nul 2> nul
+rmdir cleantf2 > nul 2> nul
+echo done
 pause

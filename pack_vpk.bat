20bded20e0e00f23ba10beb0fb6eb9bc07965a4b
JarateKing <scmacintosh@upei.ca>
2018-10-17 18:20:54 -0300

    made packing add readme

--- pack_vpk.bat
+++ pack_vpk.bat
@@ -3,7 +3,9 @@ move cfg cleantf2\
 move materials cleantf2\
 move models cleantf2\
 move scripts cleantf2\
+copy dev\ref\vpk.txt cleantf2\readme.txt
 ..\..\..\bin\vpk.exe -M cleantf2
+del cleantf2\readme.txt
 move cleantf2\cfg .\
 move cleantf2\materials .\
 move cleantf2\models .\

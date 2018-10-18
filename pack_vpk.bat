0409df8973c785d6bf33352a84fd52327dad999f
JarateKing <scmacintosh@upei.ca>
2018-10-18 10:57:06 -0300

    made vpk packing put options list into readme

--- pack_vpk.bat
+++ pack_vpk.bat
@@ -1,13 +1,24 @@
 @echo off
+:: starting
 echo preparing folder structure
 mkdir cleantf2 > nul 2> nul
 move cfg cleantf2\ > nul 2> nul
 move materials cleantf2\ > nul 2> nul
 move models cleantf2\ > nul 2> nul
 move scripts cleantf2\ > nul 2> nul
+
+:: generating readme
 copy dev\ref\vpk.txt cleantf2\readme.txt > nul 2> nul
+echo.>> cleantf2\readme.txt
+echo.>> cleantf2\readme.txt
+echo Current Options:>> cleantf2\readme.txt
+type dev\current_options.txt >> cleantf2\readme.txt
+
+:: creating vpk
 echo creating vpk
 ..\..\..\bin\vpk.exe -M cleantf2 > nul 2> nul
+
+:: regenerating folder structure
 echo regenerating folder structure
 del cleantf2\readme.txt > nul 2> nul
 move cleantf2\cfg .\ > nul 2> nul
@@ -15,5 +26,7 @@ move cleantf2\materials .\ > nul 2> nul
 move cleantf2\models .\ > nul 2> nul
 move cleantf2\scripts .\ > nul 2> nul
 rmdir cleantf2 > nul 2> nul
+
+:: done
 echo done
 pause

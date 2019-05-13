ab936bbd434970fe018533a6abdcf5ec052b13d9
JarateKing <scmacintosh@upei.ca>
2019-05-12 21:10:30 -0300

    added warning to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo THIS IS A WIP
+echo THIS CURRENTLY ONLY DOES:
+echo - FLAT TEXTURES
+echo - NOHATS
+
 dev/generators/textures_flat.sh dev/lists/linux/flat.txt "../../tf2_textures_dir.vpk" 2
 
 dev/generators/models_null.sh dev/lists/linux/nohats.txt "../../../bin/vpk.sh" "../../tf2_misc_dir.vpk"

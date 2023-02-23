97f6c344c647ac9ab627fd122616632e67c8a65f
Pyogenics <py0g3n1cs@gmail.com>
2023-02-23 20:20:15 +0000

    Linux: Added vpk script in-house

---
+++ dev/linux/vpk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH=../../../../../bin ../../../../../bin/vpk_linux32 $*
--- generate.sh
+++ generate.sh
@@ -7,4 +7,4 @@ echo - NOHATS
 
 dev/generators/textures_flat.sh dev/lists/linux/flat.txt "../../tf2_textures_dir.vpk" 2
 
-dev/generators/models_null.sh dev/lists/linux/nohats.txt "../../../bin/vpk.sh" "../../tf2_misc_dir.vpk"
+dev/generators/models_null.sh dev/lists/linux/nohats.txt "dev/linux/vpk.sh" "../../tf2_misc_dir.vpk"

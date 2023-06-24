8bfd02ac00b86187b7b890d7002146f2b5094f5b
Pyogenics <py0g3n1cs@gmail.com>
2023-06-24 15:06:10 +0100

    [LINUX] Use static HLExtract binary

--- dev/generators/textures_flat.sh
+++ dev/generators/textures_flat.sh
@@ -6,7 +6,7 @@ for line in $(cat $1); do
 			DIR=$(dirname "$line")
 			mkdir -p $DIR > /dev/null 2>&1
 			echo flat texturizing $line
-			dev/linux/HLExtract.sh -p $2 -d $line/../ -e $line -m -v -s > /dev/null 2>&1
+			dev/linux/HLExtract -p $2 -d $line/../ -e $line -m -v -s > /dev/null 2>&1
 			dev/batch/vtf-to-tga.sh $line > /dev/null 2>&1
 			for file in $DIR/*.tga; do
 				dev/batch/average-color.sh $file > /dev/null 2>&1
@@ -19,7 +19,7 @@ for line in $(cat $1); do
 		*)
 			mkdir -p $line > /dev/null 2>&1
 			echo flat texturizing $line
-			dev/linux/HLExtract.sh -p $2 -d $line/../ -e $line -m -v -s > /dev/null 2>&1
+			dev/linux/HLExtract -p $2 -d $line/../ -e $line -m -v -s > /dev/null 2>&1
 			for file in $line/*.vtf; do
 				dev/batch/vtf-to-tga.sh $file > /dev/null 2>&1
 			done
